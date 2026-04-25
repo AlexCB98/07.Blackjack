7b47ba73bd3b16012669f8ec21c4ad2e82c395f9
Alexandru <lexyus22@gmail.com>
2026-04-25 11:31:10 +0200

    Improve UI formatting and game output readability.

--- main..py
+++ main..py
@@ -7,51 +7,54 @@ want_to_play = input('Do you want to play a game of Blackjack ? Type y/n: ')
 
 while want_to_play == 'y':
 
+    print("\n" + "-" * 25)
+    print("      BLACKJACK")
+    print("-" * 25)
+
     user_cards = []
     user_cards.append(cards[0])
     user_cards.append(cards[1])
     score = sum(user_cards)
-    print(f'    Your cards: {user_cards}, current score: {score}')
+    print(f'Your cards: {user_cards}, current score: {score}')
 
     computer_cards = []
     computer_cards.append(cards[random.randint(0,12)])
     computer_score = 0
-    print(f'    Computer first card is: {computer_cards}')
+    print(f'Computer shows: {computer_cards}')
 
-    another = input('Do you want another card? Type y/n: ')
+    another = input('Draw another card? Type y/n: ')
     while True:
         if another == 'y':
             user_cards.append(cards[random.randint(0,12)])
             score = sum(user_cards)
             if score <= 21:
-                print(f'    Your cards: {user_cards}, current score: {score}')
-                print(f'    Computer first card is: {computer_cards}')
-                another = input('Do you want another card? Type y/n: ')
+                print(f'Your cards: {user_cards} (score: {score})')
+                print(f'Computer shows: {computer_cards}')
+                another = input('Draw another card? Type y/n: ')
             elif score > 21:
-                print(f'    Your cards: {user_cards}, current score: {score}')
-                print('You lose. You went over')
+                print(f'Your cards: {user_cards} (score: {score})')
+                print('\nResult: You lose. You went over')
                 break
 
         else:
-            print(f'    Your final cards: {user_cards}, final score: {score}')
+            print(f'\nYour final hand: {user_cards} (score: {score})')
             computer_cards.append(cards[random.randint(0,12)])
             computer_score = sum(computer_cards)
-            print(f'    Computer final cards: {computer_cards}, final score: {computer_score}')
+            print(f'Computer hand: {computer_cards} (score: {computer_score})')
             if score == computer_score:
-                print('         Draw.')
+                print('\nResult: Draw.')
             elif computer_score > 21:
-                print(f'    Computer final cards: {computer_cards}, final score: {computer_score}')
-                print('Computer went over. You win.')
+                print('\nResult: Computer went over. You win.')
             elif computer_score <= 21 and computer_score < score:
-                print('         You win.')
+                print('\nResult: You win.')
             else:
-                print('         You lose.')
+                print('\nResult: You lose.')
             break
-    again = input('Do you want to play a game of Blackjack ? Type y/n: ')
+    again = input('\nDo you want to play again ? Type y/n: ')
     if again == 'y':
         continue
     else:
-        print('Thanks for playing this Blackjack game. ^^')
+        print('\nThanks for playing this Blackjack game. ^^')
         break
 
 
